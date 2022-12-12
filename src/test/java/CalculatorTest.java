3d3e64a73e68f7acb39117136a7b824e807edf95
Mignonne de Groote <mignonne.de.groote@rabobank.nl>
2022-12-12 13:27:38 +0100

    test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayNameGeneration(DisplayNameGenerator.Standard.class)
+@DisplayNameGeneration(DisplayNameGenerator.class)
 public class CalculatorTest {
     Calculator calculator = new Calculator();
 
-    @DisplayName("multiply test")
+    @DisplayName("multiplication test")
     @Test
     public void testMultiply() {
         double a = -1.0;
@@ -22,6 +23,7 @@ public class CalculatorTest {
         assertEquals(-0.0, calculator.multiply(a, c));
     }
 
+    @DisplayName("division test")
     @Test
     public void testDivide() {
         double a = -1.0;
@@ -29,19 +31,21 @@ public class CalculatorTest {
         double c = 0;
 
         assertEquals(calculator.divide(a, b), -0.25);
-        assertThrows(ArithmeticException.class, () -> calculator.divide(a,c));
+        assertThrows(ArithmeticException.class, () -> calculator.divide(a, c));
     }
 
+    @DisplayName("subtraction test")
     @Test
     public void testSubtract() {
         double a = -1.0;
         double b = 4;
         double c = 0;
 
-        assertEquals( -5.0,calculator.subtract(a, b));
-        assertEquals( -1.0,calculator.subtract(a, c));
+        assertEquals(-5.0, calculator.subtract(a, b));
+        assertEquals(-1.0, calculator.subtract(a, c));
     }
 
+    @DisplayName("sum test")
     @Disabled
     @Test
     public void testSum() {
@@ -49,8 +53,8 @@ public class CalculatorTest {
         double b = 4;
         double c = 0;
 
-        assertEquals( 3.0, calculator.sum(a, b));
-        assertEquals( -1.0, calculator.sum(a, c));
+        assertEquals(3.0, calculator.sum(a, b));
+        assertEquals(-1.0, calculator.sum(a, c));
     }
 
 }
